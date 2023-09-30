bfc2d81137144dea46e6dddc732fa44b50e73f5c
Yazan Ghunaim <yazanghunaim07@gmail.com>
2023-09-30 11:22:13 +0200

    initial commit

---
+++ movieDB/movieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieDBApp.swift
+//  movieDB
+//
+//  Created by Yazan Ghunaim on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct movieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
